8d7e9b05f2926d26fb5b05276a2b9031b931573d
Kevin Tucker <kevin.tucker19@gmail.com>
2020-12-07 13:05:39 -0500

    updated index, folders, and notes.md

--- index.js
+++ index.js
@@ -3,18 +3,11 @@ const express = require("express");
 const hostname = "localhost";
 const port = 3300;
 const app = express();
-
 const serve = http.createServer(app);
 
-app.get("/", (req, res) => {
-  res.send(`
-    <h1>Welcome to the Home Page of your Future!</h1>
-    `);
-});
-
-app.get("*", (req, res) => {
-  res.send("404 Error: Page Does Not Exist");
-});
+app.use(require("./routes"));
+app.use(require("./models"));
+app.use(require("./controllers"));
 
 serve.listen(port, hostname, () => {
   console.log(`Running on port http://${hostname}:${port}`);
