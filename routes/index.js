c6d6a4ebcb024e8f7482f7716f2ecbaa4e6adea5
Kevin Tucker <kevin.tucker19@gmail.com>
2020-12-09 00:01:00 -0500

    updated gitignore index

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+router.use("/", require("./frontEnd"));
 router.use("/api", require("./api.v0"));
 router.use("/customers", require("./customers.v0"));
-
-router.get("/", (req, res) => {
-  res.send(`
-      <h1>Welcome to the Home Page of your Future!</h1>
-      `);
-});
 
 router.get("*", (req, res) => {
   res.send("404 Error: Page Does Not Exist");
