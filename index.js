902f70223a817309fa60c551d9170e4bf86b47bb
Shoel Uddin <shoeluddin@Shoels-MacBook-Pro.local>
2020-12-10 21:34:00 -0500

    updates helmet/morgan

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const hostname = "localhost";
 const port = 3300;
 const app = express();
 const serve = http.createServer(app);
+const morgan = require("morgan")
+const helmet = require("helmet")
+
+const logger = morgan('tiny')
+app.use(express.static("public"))
+app.use(helmet())
+app.use(logger)
 
 app.use(require("./routes"));
 app.use(require("./models"));
