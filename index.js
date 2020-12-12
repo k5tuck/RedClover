9f0d7b6aa633e3308a186bb62d18bcacfa5eb8b2
Kevin Tucker <kevin.tucker19@gmail.com>
2020-12-11 22:06:23 -0500

    created forms

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ const port = 3300;
 const es6Renderer = require("express-es6-template-engine");
 const app = express();
 const serve = http.createServer(app);
-const morgan = require("morgan")
-const helmet = require("helmet")
+const morgan = require("morgan");
+const helmet = require("helmet");
 
-const logger = morgan('tiny')
-app.use(express.static("public"))
-app.use(helmet())
-app.use(logger)
+const logger = morgan("tiny");
+app.use(express.static("public"));
+app.use(helmet());
+app.use(logger);
 
 app.engine("html", es6Renderer);
 app.set("views", "templates");
 app.set("view engine", "html");
 
 app.use(require("./routes"));
-app.use(require("./models"));
+// app.use(require("./models"));
 app.use(require("./controllers"));
 
 serve.listen(port, hostname, () => {
--- routes/frontEnd.js
+++ routes/frontEnd.js
@@ -14,7 +14,18 @@ router.get("/Products", (req, res) => {
   res.render("Products");
 });
 router.get("/Login", (req, res) => {
-  res.render("Login");
+  res.render("Login", {
+    partials: {
+      login_form: "/partials/loginform",
+    },
+  });
+});
+router.get("/SignUp", (req, res) => {
+  res.render("SignUp", {
+    partials: {
+      signup_form: "/partials/signupform",
+    },
+  });
 });
 
 module.exports = router;
