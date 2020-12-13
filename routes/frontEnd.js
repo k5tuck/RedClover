3f7b0919d81339623b1c1702ae1d5445490a471d
Kevin Tucker <kevin.tucker19@gmail.com>
2020-12-12 21:18:14 -0500

    updated files

--- routes/frontEnd.js
+++ routes/frontEnd.js
@@ -1,32 +1,34 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.render("Home");
 });
-router.get("/About", (req, res) => {
+router.get("/about", (req, res) => {
   res.render("About");
 });
-router.get("/Contact", (req, res) => {
+router.get("/contact", (req, res) => {
   res.render("Contact");
 });
-router.get("/Products", (req, res) => {
+router.get("/products", (req, res) => {
   res.render("Products");
 });
-router.get("/Login", (req, res) => {
+router.get("/login", (req, res) => {
   res.render("Login", {
     partials: {
       login_form: "/partials/loginform",
     },
   });
 });
-router.post("/Login", (req, res) => {
+router.post("/login", async (req, res) => {
   const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
   // Use req data to verify info against DB
   res.render("");
 });
 
-router.get("/SignUp", (req, res) => {
+router.get("/signup", (req, res) => {
   res.render("SignUp", {
     partials: {
       signup_form: "/partials/signupform",
@@ -34,20 +36,24 @@ router.get("/SignUp", (req, res) => {
   });
 });
 
-router.post("/SignUp", (req, res) => {
-  const {
-    firstName,
-    lastName,
-    street,
-    additionalStreet,
-    city,
-    state,
-    zip,
-    email,
-    phone,
-  } = req.body;
-  // Use req data for DB Here
-  res.redirect(); //Send User to Page to set Username And Password
+router.post("/signup", async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      street,
+      additionalStreet,
+      city,
+      state,
+      zip,
+      email,
+      phone,
+      ssn,
+      govtid,
+    } = req.body;
+    // Use req data for DB Here
+    res.redirect(); //Send User to Page to set Username And Password
+  } catch {}
 });
 
 module.exports = router;
