6be2e523d993f0c9605903c4e1a6eea7536754bf
Shoel Uddin <shoeluddin@Shoels-MacBook-Pro.local>
2020-12-30 19:37:39 -0500

    sessions code

--- index.js
+++ index.js
@@ -21,6 +21,20 @@ app.engine("html", es6Renderer);
 app.set("views", "templates");
 app.set("view engine", "html");
 
+//Needed for Sessions
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+app.use(session({
+    store: new FileStore(),  // no options for now
+    secret: 'asdfasdfasdfsafsafsafdasdfasdf3', // will need to be changed to  `process.env.SESSION_SECRET,` and a SESSION_SECRET added to the .env
+    saveUninitialized: false,
+    resave: true,
+    rolling: true,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}));
+
 app.use(require("./routes"));
 // app.use(require("./models"));
 // app.use(require("./controllers/controller"));
